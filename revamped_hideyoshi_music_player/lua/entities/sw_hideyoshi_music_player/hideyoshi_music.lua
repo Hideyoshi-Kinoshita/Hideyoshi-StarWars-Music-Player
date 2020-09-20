fd0fbb7bef21157a5d2b71583c508918549ba9ba
Hideyoshi-Kinoshita <67493804+Hideyoshi-Kinoshita@users.noreply.github.com>
2020-09-20 14:41:45 +1000

    Added hideyoshi_music.lua
    This File uses the Sound.Add Function to allow Garry's Mod to add the Star Wars Music into the Clients Library for use.

---
+++ revamped_hideyoshi_music_player/lua/entities/sw_hideyoshi_music_player/hideyoshi_music.lua
@@ -0,0 +1,426 @@
+AddCSLuaFile()
+
+sound.Add({
+    name = "Main Title / Rebel Blockade / Runner Medley",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Episode IV: A New Hope",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "MainTitle.mp3"
+})
+
+sound.Add({
+    name = "Duel of the Fates",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars: Prequal Trilogy",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "DuelOfTheFates.mp3"
+})
+
+sound.Add({
+    name = "Binary Sunset/The Force Theme",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Episode IV: A New Hope",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "BinarySunset.mp3"
+})
+
+sound.Add({
+    name = "The Birth of the Twins and Padmé's Destiny",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Episode III: Revenge of the Sith",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "TheBirthoftheTwinsandPadmesDestiny.mp3"
+})
+
+sound.Add({
+    name = "Across the Stars (Love Theme)",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Episode II: Attack of the Clones",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "AcrossTheStars.mp3"
+})
+
+sound.Add({
+    name = "Hope",
+    arist = "Michael Giacchino",
+    movie = "Rogue One: A Star Wars Story",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "Hope.mp3"
+})
+
+sound.Add({
+    name = "The Imperial Suite",
+    arist = "Michael Giacchino",
+    movie = "Rogue One: A Star Wars Story",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "TheImperialSuite.mp3"
+})
+
+sound.Add({
+    name = "Your Father Would Be Proud",
+    arist = "Michael Giacchino",
+    movie = "Rogue One: A Star Wars Story",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "YourFatherWouldBeProud.mp3"
+})
+
+sound.Add({
+    name = "Anakin's Betrayal",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Episode III: Revenge of the Sith",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "AnakinsBetrayal.mp3"
+})
+
+sound.Add({
+    name = "The Imperial March (Lord Vader's Theme)",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Episode V: The Empire Strikes Back",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "TheImperialMarch.mp3"
+})
+
+sound.Add({
+    name = "Cantina Band",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Episode IV: A New Hope",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "CantinaBand.mp3"
+})
+
+sound.Add({
+    name = "The Mandalorian",
+    arist = "Ludwig Göransson",
+    movie = "Star Wars: The Mandalorian (Chapter 1)",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "TheMandalorian.mp3"
+})
+
+sound.Add({
+    name = "The Mandalorian (Orchestral Version)",
+    arist = "Ludwig Göransson",
+    movie = "Star Wars: The Mandalorian (Chapter 7)",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "TheMandalorianV2.mp3"
+})
+
+sound.Add({
+    name = "Thrawn's Web",
+    arist = "Kevin Kiner/John Williams",
+    movie = "Star Wars: Rebels",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "ThrawnsWeb.mp3"
+})
+
+sound.Add({
+    name = "Attack of the Clones",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Epsiode II: Attack Of The Clones",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "AttackOfTheClones.mp3"
+})
+
+sound.Add({
+    name = "Indian Pop source / Hondo Ohnaka's Theme",
+    arist = "Kevin Kiner",
+    movie = "Star Wars: Rebels",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "IndianPopSource.mp3"
+})
+
+sound.Add({
+    name = "Love Pledge and the Arena (Jedi Temple Edit)",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Epsiode II: Attack Of The Clones",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "JediTempleMarch.mp3"
+})
+
+sound.Add({
+    name = "Bounty Hunter Pursuit",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Epsiode II: Attack Of The Clones",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "BountyHunterPursuit.mp3"
+})
+
+sound.Add({
+    name = "It's Over Now",
+    arist = "Kevin Kiner",
+    movie = "Star Wars: Rebels",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "ItsOverNow.mp3"
+})
+
+sound.Add({
+    name = "The Occupation of Balmorra / Revan's Theme",
+    arist = "Gordy Haab/Mark Griskey",
+    movie = "Star Wars: The Old Republic",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "TheOccupationOfBalmorra.mp3"
+})
+
+sound.Add({
+    name = "Revan's Theme (TOOB Edit)",
+    arist = "Gordy Haab/Mark Griskey",
+    movie = "Star Wars: The Old Republic (Game)",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "RevansTheme.mp3"
+})
+
+sound.Add({
+    name = "Republic Commando Theme (Vode An)",
+    arist = "Jesse Harlin/Sameul Kim",
+    movie = "Unknown",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "RepublicCommandoTheme.mp3"
+})
+
+sound.Add({
+    name = "Black Thunder",
+    arist = "The Hu",
+    movie = "Star Wars: Jedi - Fallen Order (Game)",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "FallenOrderTheHu.mp3"
+})
+
+sound.Add({
+    name = "Valkorion Theme",
+    arist = "Gordy Haab/Mark Griskey",
+    movie = "Star Wars: The Old Republic (Game)",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "ValkorianTheme.mp3"
+})
+
+sound.Add({
+    name = "Anakin's Theme",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Epsiode I: Phantom Menance",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "AnakinsTheme.mp3"
+})
+
+sound.Add({
+    name = "I Have An Idea (Rogue One Motif)",
+    arist = "Michael Giacchino",
+    movie = "Rogue One: A Star Wars Story",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "IHaveAnIdea.mp3"
+})
+
+sound.Add({
+    name = "The Master Switch",
+    arist = "Michael Giacchino",
+    movie = "Rogue One: A Star Wars Story",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "TheMasterSwitch.mp3"
+})
+
+sound.Add({
+    name = "Glory of the Empire (Imperial Anthem)",
+    arist = "Kevin Kiner",
+    movie = "Star Wars: Rebels",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "GloryOfTheEmpire.mp3"
+})
+
+sound.Add({
+    name = "Nurse and Protect",
+    arist = "Ludwig Göransson",
+    movie = "Star Wars: The Mandalorian (Chapter 8)",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "NurseAndProtect.mp3"
+})
+
+sound.Add({
+    name = "Bounty Droid",
+    arist = "Ludwig Göransson",
+    movie = "Star Wars: The Mandalorian (Chapter 8)",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "BountyDroid.mp3"
+})
+
+sound.Add({
+    name = "The Battle Of Endor I (Medley)",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Epsiode VI: Return of the Jedi",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+
+    sound = "TheBattleOfEndorI.mp3"
+})
+
+sound.Add({
+    name = "The Battle Of Endor II (Medley)",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Epsiode VI: Return of the Jedi",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "TheBattleOfEndorII.mp3"
+})
+
+sound.Add({
+    name = "The Battle Of Endor III (Medley)",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Epsiode VI: Return of the Jedi",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+
+    sound = "TheBattleOfEndorIII.mp3"
+})
+
+sound.Add({
+    name = "Revisiting Snoke",
+    arist = "John Williams",
+    movie = "Star Wars Epsiode VII: The Last Jedi",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+
+    sound = "RevisitingSnoke.mp3"
+})
+
+sound.Add({
+    name = "The Ewebb",
+    arist = "Ludwig Göransson",
+    movie = "Star Wars: The Mandalorian (Chapter 8)",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "TheEwebb.mp3"
+})
+
+sound.Add({
+    name = "BF2 Gameplay Trailer Theme",
+    arist = "L'Orchestra Cinématique",
+    movie = "Star Wars: New Battlefront 2 (Game)",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+
+    sound = "BF2GameplayTrailerTheme.mp3"
+})
+
+sound.Add({
+    name = "Final Trailer Theme",
+    arist = "John Willaims",
+    movie = "Star Wars Episode IX: The Rise of Skywalker",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "FinalTrailerTheme.mp3"
+})
+
+sound.Add({
+    name = "Rey's Theme",
+    arist = "John Willaims",
+    movie = "Star Wars Episode IX: The Rise of Skywalker",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+
+    sound = "ReysTheme.mp3"
+})
+
+sound.Add({
+    name = "March of the Resistance",
+    arist = "John Willaims",
+    movie = "Star Wars Episode VII: The Force Awakens",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "MarchOfTheResistance.mp3"
+})
+
+sound.Add({
+    name = "I'm Han Solo",
+    arist = "Gordy Haab/Kyle Newmaster",
+    movie = "Star Wars: Kinect",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "ImHanSolo.mp3"
+})
+
+sound.Add({
+    name = "Throme Room / End Title",
+    arist = "John Williams/London Symphony Orchestra",
+    movie = "Star Wars Episode IV: A New Hope",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "TheThroneRoom.mp3"
+})
+
+sound.Add({
+    name = "Moff Gideon Suite",
+    arist = "Ludwig Göransson",
+    movie = "Star Wars: The Mandalorian (Chapter 8)",
+    channel = CHAN_AUTO,
+	volume = 1.0,
+	level = 80,
+    sound = "MoffGideonSuite.mp3"
+})
